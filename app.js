0bc6a9abeafca2c2eef52e41e1474e5e9e3bee01
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-28 10:49:00 -0400

    setup: determine winner

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const getRandomValue = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min) + min);
 };
 
 let currentRound = 0;
@@ -10,6 +10,7 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
+            winner: null,
         }
     },
     computed: {
@@ -23,10 +24,26 @@ const app = Vue.createApp({
             return this.currentRound % 3;
         }
     },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = "monster"
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
+        }
+    },
     methods: {
         attackMonster() {
             this.currentRound++;
-            this.monsterHealth = this.monsterHealth - getRandomValue(5, 12);
+            this.monsterHealth -= getRandomValue(5, 12);
             this.attackPlayer();
         },
         attackPlayer() {
@@ -36,6 +53,17 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+
+            if (healValue + this.playerHealth > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
             this.attackPlayer();
         }
     }
