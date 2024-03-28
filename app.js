de4ba580396745dbf1dc8f91efbac0b63af29692
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-28 10:14:31 -0400

    setup: add attackPlayer method

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+const getRandomValue = (min, max) => {
+    Math.floor(Math.random() * (max - min)) + min;
+};
+
 const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth:100
+            monsterHealth: 100
         }
     },
     methods: {
         attackMonster() {
-            this.monsterHealth -= Math.floor(Math.random() * (12 - 5)) + 5; // random dmg between 5-12.
+            this.monsterHealth -= getRandomValue(5, 12);
+            this.attackPlayer();
         },
+        attackPlayer() {
+            this.playerHealth -= getRandomValue(8, 15);
+        }
     }
 })
 
