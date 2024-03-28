1591a668db01a7fbb2d9ba5b8fbc19713f39e920
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-28 10:36:59 -0400

    setup: special attack only once every 3 rounds

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const getRandomValue = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+let currentRound = 0;
+
 const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0,
         }
     },
     computed: {
@@ -15,15 +18,25 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return { width: this.playerHealth + '%' }
+        },
+        isDisabled() {
+            return this.currentRound % 3;
         }
     },
     methods: {
         attackMonster() {
+            this.currentRound++;
             this.monsterHealth = this.monsterHealth - getRandomValue(5, 12);
             this.attackPlayer();
         },
         attackPlayer() {
             this.playerHealth -= getRandomValue(8, 15);
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 })
